2eceb67ae42e643ee284c586414ceaec22ca7c61
Raki Rahman <mdrrahman@microsoft.com>
2021-11-28 17:45:26 -0500

    Create TF pipeline

--- commands.sh
+++ commands.sh
@@ -1,14 +1,16 @@
 # Create a TF namespace for Kubernetes operator
-kubectl create namespace tfops
+kubectl create namespace sqlmiops
 
 # Update the credentials.example file and rename credentials then create a K8s secret
-kubectl create -n tfops secret generic terraformrc --from-file=credentials=tfops/credentials
+kubectl create -n sqlmiops secret generic terraformrc --from-file=credentials=sqlmiops/credentials
 
 # Create the follow env-variables in shell prior to running this script
 export client_id='<your-client-id>'
 export client_secret='<your-client-key>'
 export tenant_id='<your-tenant>'
 export subscription_id='<your-sub-id>'
+export sqlmi_administrator_login='<your-sql-login>'
+export sqlmi_administrator_password='<your-sql-password>'
 
 # Create an env-variables configmap for non-secret values
 cat > env-variables.yaml <<EOF
@@ -24,23 +26,22 @@ data:
 EOF
 
 # Deployment Secrets
-kubectl create secret -n tfops generic workspacesecrets --from-literal=ARM_CLIENT_SECRET=$client_secret \
---from-literal=sqlmi_administrator_login=$sqlmi_administrator_login \
---from-literal=sqlmi_administrator_password=$sqlmi_administrator_password
+kubectl create secret -n sqlmiops generic workspacesecrets \
+         --from-literal=ARM_CLIENT_SECRET=$client_secret
 
 # Create the config map
-kubectl apply -f env-variables.yaml -n tfops
+kubectl apply -f env-variables.yaml -n sqlmiops
 
 # Install the terraform operator
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm search repo hashicorp/terraform --devel
-helm install --devel --namespace tfops hashicorp/terraform --generate-name
+helm install --devel --namespace sqlmiops hashicorp/terraform --generate-name
 
 # Apply sqlmi.yaml to create the workspace and resources
-kubectl apply -f tfops/sqlmi.yaml -n tfops
+kubectl apply -f sqlmiops/sqlmi.yaml -n sqlmiops
 
 # Validate the process
-kubectl describe workspace -n tfops sqlmi
+kubectl describe workspace -n sqlmiops sqlmi
 
 # Delete the workspace
-kubectl delete -f tfops/sqlmi.yaml -n tfops
+kubectl delete -f sqlmiops/sqlmi.yaml -n sqlmiops
